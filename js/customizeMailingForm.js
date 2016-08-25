21f6e3b85aca9e906e3e5ad11b30bf334b3c0cd2
Alain Benbassat <alain@businessandcode.eu>
2016-08-25 19:05:54 +0200

    hide track response div in mailing

--- js/customizeMailingForm.js
+++ js/customizeMailingForm.js
@@ -1,6 +1,11 @@
 cj(document).ajaxComplete(
-  function() {
-    // remove div "send test e-mail to group"
-    cj("div.preview-group").hide();
+  function(event, xhr, settings) {
+    if (settings.url.indexOf("action=getfields") > 0) {
+      // remove div "send test e-mail to group"
+      cj("div.preview-group").hide();
+    
+      // remove elements in track response
+      cj("#tab-response").children().hide();
+    }
   }
 );
