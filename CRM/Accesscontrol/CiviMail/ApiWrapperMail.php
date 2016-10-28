de5ad7bdd13b5e75e09de998839cd09fd2c22e3a
Jaap Jansma <jaap@edeveloper.nl>
2016-10-28 10:11:18 +0200

    Fixed regression with new mailing for landelijke gebruikers

--- CRM/Accesscontrol/CiviMail/ApiWrapperMail.php
+++ CRM/Accesscontrol/CiviMail/ApiWrapperMail.php
@@ -19,18 +19,18 @@ class CRM_Accesscontrol_CiviMail_ApiWrapperMail implements API_Wrapper {
    *   modified $result
    */
   public function toApiOutput($apiRequest, $result) {
-    $values = array();
+
     $allowedMailingIds = CRM_Mailing_BAO_Mailing::mailingACLIDs();
-    //var_dump($allowedMailingIds);
-    if ($allowedMailingIds) {
+    if (is_array($allowedMailingIds)) {
+      $values = array();
       foreach ($result['values'] as $mid => $mailing) {
         if (in_array($mailing['id'], $allowedMailingIds)) {
           $values[$mid] = $mailing;
         }
       }
+      $result['values'] = $values;
+      $result['count'] = count($result['values']);
     }
-    $result['values'] = $values;
-    $result['count'] = count($result['values']);
     return $result;
   }
 }
